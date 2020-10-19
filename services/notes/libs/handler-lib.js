59f9edb1c9ccb33ae6bf6029f536cd40b05c21e6
7ubuntu user <ubunturemote@ip-172-31-2-214.eu-north-1.compute.internal>
2020-10-19 15:02:37 +0300

    Reorganizing the repo

---
+++ services/notes/libs/handler-lib.js
@@ -0,0 +1,28 @@
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+       // Print out the full error
+      console.log(e);
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      },
+    };
+  };
+};

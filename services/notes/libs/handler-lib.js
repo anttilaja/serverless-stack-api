19c3d525573e67c9b64a1f9ad98fa962e0b696e3
7ubuntu user <ubunturemote@ip-172-31-2-214.eu-north-1.compute.internal>
2020-11-12 10:48:38 +0200

    adding erroro logging

--- services/notes/libs/handler-lib.js
+++ services/notes/libs/handler-lib.js
@@ -1,14 +1,20 @@
+import * as debug from "./debug-lib";
+
 export default function handler(lambda) {
   return async function (event, context) {
     let body, statusCode;
+
+    // Start debugger
+    debug.init(event, context);
 
     try {
       // Run the Lambda
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
-       // Print out the full error
-      console.log(e);
+      // Print debug messages
+      debug.flush(e);
+
       body = { error: e.message };
       statusCode = 500;
     }
@@ -20,9 +26,7 @@ export default function handler(lambda) {
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials": true,
-        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
       },
     };
   };
-};
+}
